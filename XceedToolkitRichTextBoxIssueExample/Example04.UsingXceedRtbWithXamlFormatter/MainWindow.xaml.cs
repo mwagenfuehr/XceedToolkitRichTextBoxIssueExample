f1ba9982fabae1e125d49f66173281a8b8d2288b
Martin Wagenführ <martin.wagenfuehr@schoenhofer.de>
2019-03-22 13:59:10 +0100

    Added Example 04

---
+++ XceedToolkitRichTextBoxIssueExample/Example04.UsingXceedRtbWithXamlFormatter/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Documents;
+using Xceed.Wpf.Toolkit;
+using MessageBox = System.Windows.MessageBox;
+
+namespace Example04.UsingXceedRtbWithXamlFormatter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            RichTextBox.TextFormatter = new XamlFormatter();
+        }
+
+        private void ButtonGetRtf_Click(object sender, RoutedEventArgs e)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var txtRange = new TextRange(RichTextBox.Document.ContentStart,
+                    RichTextBox.Document.ContentEnd);
+
+                txtRange.Save(ms, DataFormats.Rtf);
+                ms.Seek(0, SeekOrigin.Begin);
+
+                using (var sr = new StreamReader(ms))
+                {
+                    MessageBox.Show(sr.ReadToEnd());
+                }
+            }
+        }
+    }
+}
