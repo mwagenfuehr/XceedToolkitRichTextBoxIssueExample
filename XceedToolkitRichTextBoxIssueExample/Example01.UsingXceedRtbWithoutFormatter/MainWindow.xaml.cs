fdbde5448de5e34d4121ec793cb3705b252f4014
Martin Wagenführ <martin.wagenfuehr@schoenhofer.de>
2019-04-15 08:51:27 +0200

    Added logging and tracing for first-chance-exceptions

--- XceedToolkitRichTextBoxIssueExample/Example01.UsingXceedRtbWithoutFormatter/MainWindow.xaml.cs
+++ XceedToolkitRichTextBoxIssueExample/Example01.UsingXceedRtbWithoutFormatter/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Example01.UsingXceedRtbWithoutFormatter
 {
---
+++ XceedToolkitRichTextBoxIssueExample/Example01.UsingXceedRtbWithoutFormatter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+
+// ReSharper disable once IdentifierTypo
+namespace Example01.UsingXceedRtbWithoutFormatter
+{
+    internal static class Program
+    {
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            AppDomain.CurrentDomain.FirstChanceException += (sender, eventArgs) =>
+            {
+                Trace.TraceError(eventArgs.Exception.ToString());
+                File.AppendAllText("output.log", eventArgs.Exception.ToString() + Environment.NewLine);
+            };
+            
+            return new App().Run(new MainWindow());
+        }
+    }
+}
